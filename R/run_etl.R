80fff5320017e754e49ad8e6dd1effc3e1b09db7
Aleksander Dietrichson <dietrichson@gmail.com>
2021-02-28 20:12:28 -0300

    etl updates added ships.RDS

--- R/run_etl.R
+++ R/run_etl.R
@@ -6,18 +6,21 @@
 #' @importFrom readr write_rds
 #' @return Save a RDS called ships.RDS
 #' @noRd
-run_etl <- function(path_ship_csv,  output_path = here::here("inst","data","ships.RDS")){
-  output_path <- here::here("inst","data","ships.RDS")
-  if(!dir.exists(output_path)){
-    stop("Output directory not found.", output_path)
+run_etl <- function(path_ship_csv,  output_path = here::here("inst","app","shipsdata","ships.RDS")){
+ 
+  if(!dir.exists(dirname(output_path))){
+    stop("Output directory not found: ", dirname(output_path))
   }
   
   if(!file.exists(path_ship_csv)){
     stop("Input file not found.")
   }
   
-  ships_data <- read_csv(path_ship_csv)
+  ships_data <- readr::read_csv(path_ship_csv)
+  # cleanse
+  ships_data <- etl_cleanse_ships_data(ships_data)
+  # Do the pre-calculations
   final <- etl_appsilon_requirement(ships_data)
-  write_rds(final, output_path)
-
+  
+  readr::write_rds(final, output_path)
 }
