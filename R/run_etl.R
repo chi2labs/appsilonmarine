eeb177563ee132c934058e76a2b740d1f9370d09
Pablo <pablodp84@gmail.com>
2021-02-28 14:30:47 -0300

    #2 Refactoring function

--- R/run_etl.R
+++ R/run_etl.R
@@ -1,13 +1,14 @@
 #' Run Appsilon ETL process
 #'
+#' @param Path to origin csv with ships observations.
 #' @importFrom readr read_csv
 #' @importFrom readr write_rds
 #' @return Save a RDS called ships.RDS
-#' @export
-run_etl <- function(){
+#' @noRd
+run_etl <- function(path_ship_csv){
 
-  ships_data <- read_csv("inst/data/ships.csv")
+  ships_data <- read_csv(path_ship_csv)
   final <- etl_appsilon_requirement(ships_data)
-  write_rds(final, "inst/data/ships.RDS")
+  write_rds(final, system.file("data/ships.RDS", package="appsilonmarine"))
 
 }
