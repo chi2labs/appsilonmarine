28e98e91ceb7ae708b96cf27df7ce7ed9937a4b4
Pablo <pablodp84@gmail.com>
2021-02-28 02:46:11 -0300

    Bug fixed

--- R/app_server.R
+++ R/app_server.R
@@ -6,6 +6,6 @@
 #' @noRd
 app_server <- function( input, output, session ) {
   # Load data previously analyzed.
-  ships <- read_rds("inst/data/ships.RDS")
+  ships <- read_rds(system.file("data/ships.RDS", package="appsilonmarine"))
   callModule(mod_marine_server, "marine_ui_1", ships = ships)
 }
